f47d6351cb7ac9cbc6bba9fac85e9e6bc3313364
Long Pham <longpth.vn@gmail.com>
2024-07-13 16:12:31 +0700

    Added About page.

--- GeminiChessAnalysis/AppShell.xaml.cs
+++ GeminiChessAnalysis/AppShell.xaml.cs
@@ -13,10 +13,16 @@ namespace GeminiChessAnalysis
             InitializeComponent();
             //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute("AboutPage", typeof(AboutPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
+            // Navigate to the About page
+            await Shell.Current.GoToAsync("AboutPage");
+
+            // Collapse the Flyout menu
+            Shell.Current.FlyoutIsPresented = false;
         }
     }
 }
